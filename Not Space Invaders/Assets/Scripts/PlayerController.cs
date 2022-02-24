b59cf00cb480cbb91ffc90ba1ae4e5d3b2c77571
Sixcha <paulbora_akcan@hotmail.com>
2022-02-24 22:50:56 +0100

    Made a player controller

--- Not Space Invaders/Assets/Scripts/PlayerController.cs
+++ Not Space Invaders/Assets/Scripts/PlayerController.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float Speed;
+    private Rigidbody2D rb;
+    private Vector2 Direction;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float directionX = Input.GetAxis("Horizontal");
+        Debug.Log(directionX);
+        float directionY = Input.GetAxis("Vertical");
+        Direction = new Vector2(directionX, directionY).normalized;
     }
 
     void FixedUpdate()
     {
-
+        rb.velocity = new Vector2(Direction.x * Speed, Direction.y * Speed);
     }
 }
