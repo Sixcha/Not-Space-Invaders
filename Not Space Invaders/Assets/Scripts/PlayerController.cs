8940d2e34913d0569a7a1455de7468dd9cea6f2a
Sixcha <paulbora_akcan@hotmail.com>
2022-02-25 21:12:05 +0100

    Fixed player movement

--- Not Space Invaders/Assets/Scripts/PlayerController.cs
+++ Not Space Invaders/Assets/Scripts/PlayerController.cs
@@ -17,9 +17,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xInput = Input.GetAxis("Horizontal");
+        float xInput = Input.GetAxisRaw("Horizontal");
         Debug.Log(xInput);
-        float yInput = Input.GetAxis("Vertical");
+        float yInput = Input.GetAxisRaw("Vertical");
         Debug.Log(yInput);
         movement = new Vector2 (xInput, yInput);
 
@@ -28,29 +28,12 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        //MovementWithRigidBodyAddForce(movement);
-        //MovementWithRigidbodyMovePosition(movement);
-        //MovementWithRigidBodyVelocity(movement);
+
     }
 
     // It overrides any physics that is used on the object which means object will not clash to other objects and get effected by it.
     void MovementWithTranslate(Vector2 direction)
     {
         transform.Translate(direction * Time.deltaTime * speed);
-    }
-
-    void MovementWithRigidBodyAddForce(Vector2 direction)
-    {
-        rb.AddForce(direction * speed);
-    }
-
-    void MovementWithRigidBodyVelocity(Vector2 direction)
-    {
-        rb.velocity = direction * speed;
-    }
-
-    void MovementWithRigidbodyMovePosition(Vector2 direction)
-    {
-        rb.MovePosition((Vector2)transform.position + (direction * speed * Time.deltaTime));
     }
 }
