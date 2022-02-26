44452abcf053289cb6ecebaf8f99451cd60fc379
Sixcha <paulbora_akcan@hotmail.com>
2022-02-26 19:19:52 +0100

    -Created rough main menu
    -Fixed a bug

--- Not Space Invaders/Assets/Scripts/PlayerProjectile.cs
+++ Not Space Invaders/Assets/Scripts/PlayerProjectile.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerProjectile : MonoBehaviour
 {
     private Vector2 firingPoint;
+    public float range = 5f;
 
     [SerializeField]
     private float projectileSpeed;
@@ -26,7 +27,11 @@ public class PlayerProjectile : MonoBehaviour
 
     void MoveProjectile()
     {
-        if(Vector2.Distance(firingPoint, transform.position) > maxProjectileDistance)
+        /* if(Vector2.Distance(firingPoint, transform.position) > maxProjectileDistance)
+            Destroy(this.gameObject);
+        else
+            transform.Translate(Vector2.up * projectileSpeed * Time.deltaTime); */
+        if(transform.position.y > range)
             Destroy(this.gameObject);
         else
             transform.Translate(Vector2.up * projectileSpeed * Time.deltaTime);
