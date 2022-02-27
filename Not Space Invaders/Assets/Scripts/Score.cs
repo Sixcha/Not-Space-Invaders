a036f559d4146f1c3f29ae31f0a3decdf48f4f0c
Sixcha <paulbora_akcan@hotmail.com>
2022-02-27 21:12:54 +0100

    -Created color gradient and named both of them
    -Made a scoreboard that tracks score based on time

---
+++ Not Space Invaders/Assets/Scripts/Score.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class Score : MonoBehaviour
+{
+    //public GameObject player;
+    public TextMeshProUGUI scoreText;
+    public float score = 0f;
+    public int realScore;
+    public string scoreString;
+
+    void Start()
+    {
+        scoreText = GetComponent<TextMeshProUGUI>();
+        scoreText.text = "Score : 0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score += Time.deltaTime; 
+        realScore = (int)Math.Floor(score) * 50;
+        scoreString = realScore.ToString();
+        scoreText.text = $"Score : {scoreString}";
+    }
+}
